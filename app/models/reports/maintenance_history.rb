d1bebb14f3a5ab8a277d6ce72ec4652c5c447876
Vlad Khomich <khomich.vlad@gmail.com>
2011-07-24 00:49:40 +0300

    adding more reports

---
+++ app/models/reports/maintenance_history.rb
@@ -0,0 +1,32 @@
+class MaintenanceHistory < ActiveRecord::Base
+
+  # since we didn't want to create views right in the database we had to invent some conventions :)
+  # The idea is to manipulate the self::View query as it was a table( Please check lib/AbstractTable )
+  # The actual conventions are:
+  #  * Aliases for each field ( underscored, to make it look native )
+  #  * Alias for the entire query and set_table_name ( should be the same as set_table_name value )
+  #  * SQL string must be assigned to VIEW constant
+  #  * Please include AbstractTable in the end
+  #  * set_primary_key is needed for the ORDER BY purposes
+
+  set_table_name(:maintenance_history_view)
+  set_primary_key(:serial_number)
+
+  VIEW =
+      <<EOF
+-- Maintenance History
+
+(select locations.name as 'location',
+       devices.placement as 'placement',
+       devices.serial_number as 'serial_number',
+       device_check_records.created_at as 'inspection_date',
+       device_check_records.name_checker as 'inspector_name',
+       device_check_records.notes as 'issues'
+from devices
+inner join locations on devices.location_id = locations.id
+inner join device_check_records on devices.id = device_check_records.device_id) AS maintenance_history_view
+EOF
+
+  include AbstractTable
+
+end
---
+++ app/models/reports/user_training.rb
@@ -0,0 +1,32 @@
+class UserTraining < ActiveRecord::Base
+
+  # since we didn't want to create views right in the database we had to invent some conventions :)
+  # The idea is to manipulate the self::View query as it was a table( Please check lib/AbstractTable )
+  # The actual conventions are:
+  #  * Aliases for each field ( underscored, to make it look native )
+  #  * Alias for the entire query and set_table_name ( should be the same as set_table_name value )
+  #  * SQL string must be assigned to VIEW constant
+  #  * Please include AbstractTable in the end
+  #  * set_primary_key is needed for the ORDER BY purposes
+
+  set_table_name(:user_trainings_view)
+  set_primary_key(:name)
+
+  VIEW =
+      <<EOF
+-- User Trainings
+
+(select CONCAT(users.first_name, ' ', users.last_name) as 'name',
+       locations.name as 'location',
+       training_programs.name as 'training_type',
+       training_records.expiring_on as 'certification_expiration',
+       DATEDIFF(CURDATE(), DATE(training_records.expiring_on)) as 'days_until_due'
+from users
+inner join locations on users.location_id = locations.id
+inner join training_records on users.id = training_records.user_id
+inner join training_programs on training_records.training_program_id = training_programs.id) AS user_trainings_view
+EOF
+
+  include AbstractTable
+
+end
