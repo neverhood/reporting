0a840b25ab23efbb916d99419048fae198f52c62
Vlad Khomich <khomich.vlad@gmail.com>
2011-07-25 14:03:17 +0300

    started working on filters

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,19 @@
 module ReportsHelper
+
+  # Filters
+
+  def filters_for field
+
+  end
+
+  private
+
+  def filters_for_all
+    [:is_null, :is_not_null]
+  end
+
+  def filters_for_string
+    filters_for_all + [:equals, :starts_with, :ends_with, :contains]
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
     selectors.reportFieldsContainer = $('div#report-fields');
     selectors.reportFields = $(selectors.reportFieldsContainer).find('input[type="checkbox"]');
     selectors.orderBy = $('select#report_order_by');
+    selectors.filtersForFields = $('select#report_filters_field');
 
     utils.checkedReportFields = function() {
         return selectors.reportFields.filter(':checked');
@@ -35,27 +36,35 @@ $(document).ready(function() {
 
     // ORDER BY Fields Selection
     selectors.reportFields.click(function() {
+        // If user unchecks one of the fields - hide the corresponding ORDER BY and FILTER FOR options
+        // If the corresponding option is selected - select another for for user
+        // When user checks the checkbox - show the option back
         var $this = $(this),
                 checked = $this.prop('checked'),
-                option = selectors.orderBy.find('option[value="' + $this.attr('data-field-name') + '"]'),
-                firstCheckedCheckbox = function() {
-                    return $.reporting.selectors.reportFieldsContainer.
-                            find('input[type="checkbox"]').filter(':checked').first();
+                fieldName = $this.attr('data-field-name'),
+                orderByOption = selectors.orderBy.find('option[value="' + fieldName + '"]'),
+                filterForOption = selectors.filtersForFields.find('option[value="' + fieldName + '"]'),
+                options = [orderByOption, filterForOption],
+                synchronizeFields = function() {
+                    var field = $.reporting.selectors.reportFieldsContainer.
+                            find('input[type="checkbox"]').filter(':checked').first().attr('data-field-name');
+                    if (orderByOption.is(':selected')) selectors.orderBy.val(field);
+                    if (filterForOption.is(':selected')) selectors.filtersForFields.val(field);
                 };
 
-        if (option.is(':selected') && !checked) {
+        if ((orderByOption.is(':selected') || filterForOption.is(':selected'))  && !checked) {
             // User doesn't want the current 'ORDER BY' field to be included into report.
             // Select closest field for him and hide the current option
-            selectors.orderBy.val(firstCheckedCheckbox().attr('data-field-name'));
-            option.hide();
-
+            synchronizeFields();
+            $.each(options, function() {this.hide()});
         } else {
-            checked? option.show() : option.hide();
+            checked? $.each(options, function() {this.show()}) :
+                    $.each(options, function() {this.hide()});
         }
 
         if ( utils.checkedReportFields().length == 0 ) {
             $this.prop('checked', true);
-            option.show().attr('selected', true);
+            $.each(options, function() {this.show().attr('selected', true)});
         }
     });
 
