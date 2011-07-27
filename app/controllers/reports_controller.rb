bac5f788ef46063530fbd00930502ab0e1af452b
neveragny <neveragny@gmail.com>
2011-07-27 09:41:32 +0300

    export to csv

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,10 +4,12 @@ class ReportsController < ApplicationController
 
   before_filter :valid_report_type, :only => :customize
   before_filter :valid_params, :only => :create
+  before_filter :show_params, :only => :create
 
   layout Proc.new { |controller| controller.request.xhr?? false : 'application' }
 
   ITEMS_PER_PAGE = 24
+
 
   def index
     @report = Report.new
@@ -22,46 +24,49 @@ class ReportsController < ApplicationController
     @page = (params[:report][:page] ||= 1).to_i
     offset = (@page - 1) * ITEMS_PER_PAGE
     @report_engine = Report::TYPES[params[:report][:type].to_sym]
-    @objects = @report_engine.select(@fields_to_select).
-        from(@report_engine.view).where(@filters).order(@order).
-        limit(ITEMS_PER_PAGE).offset(offset).all
-    @amount = @report_engine.select(@fields_to_select).
-        from(@report_engine.view).where(@filters).count
-    @last_page = @amount/ITEMS_PER_PAGE + 1
-
-    Rails.logger.debug "#{@fields_to_select} <<<<<<<<<< #{@report_engine}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 
 
     respond_to do |format|
       format.js {
+        @objects = @report_engine.select(@fields_to_select).
+                from(@report_engine.view).where(@filters).order(@order).
+                limit(ITEMS_PER_PAGE).offset(offset).all
+            @amount = @report_engine.select(@fields_to_select).
+                from(@report_engine.view).where(@filters).count
+            @last_page = @amount/ITEMS_PER_PAGE + 1
         if params[:report][:csv] == "false"
           render :json => {:table => render_to_string('reports/_report.erb'), :params => params}
         else
           render :update do |page|
-            csv_string = CSV.generate do |cs|
-              cs << "hello this is first line".split
-              @objects.each do |item|
-                cs << item.attrs.values.each
-              end
-#              params[:objects].each do |item|
-##                Rails.logger.debug "#{item[0]}"
-#                  cs << [item[0], item[1] ]#@fields_to_select
-#              end
-            end
+                page.redirect_to :controller => :reports,
+                             :action => "file_sender",
+                             :report_type => params[:report][:type],
+                             :fields_to_select => @fields_to_select,
+                             :order => @order,
+                             :filters => @filters
 
-#          page.redirect_to :controller => :reports, :action => "file_sender", :param1 => csv_string
-           page.redirect_to csv_string, :type => "text/csv", :filename => 'olol.csv', :disposition => 'attachment'
+          end
         end
-        end }
+      }
     end
 
   end
 
   def file_sender()
-    Rails.logger.debug "PARAMS:   #{params[:param1]} ==============="
+    @report_engine = Report::TYPES[params[:report_type].to_sym]  #yes, yes.. not so much DRY but who cares at 9am  ^_^
+    @objects = @report_engine.select(params[:fields]).
+        from(@report_engine.view).where(params[:filters]).order(params[:order]).all
 
-    send_data csv_string, :type => "text/csv", :filename => 'olol.csv', :disposition => 'attachment'
-    #render_csv "users-#{Time.now.strftime("%d%m%Y")}"
+    csv_string = CSV.generate do |cs|
+      cs << "hello this is first line".split
+      @objects.each do |item|
+        cs << item.attrs.values.each
+      end
+    end
+
+    send_data csv_string, :type => "text/csv",
+              :filename => "params[:report_engine]_report_#{Time.now.strftime("%d%m%Y")}",
+              :disposition => 'attachment'
   end
 
   private
@@ -127,6 +132,10 @@ class ReportsController < ApplicationController
         end
       end
   string
+  end
+
+  def show_params
+    Rails.logger.debug "PARAMS #{params}      ################before_filter"
   end
 
 end
