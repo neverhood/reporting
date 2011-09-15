74c53361af92c8ba8afb301d73d8cf6df6e9a603
Vlad Khomich <khomich.vlad@gmail.com>
2011-09-15 13:42:04 +0300

    drag-n-drop enchanced

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,7 @@ String.prototype.trim = function() {
     return this.replace(/^\s+|\s+$/g,"");
 };
 
-Array.prototype.equals = function(array) { 
+Array.prototype.equals = function(array) {
     var result = true;
 
     for ( element in this ) {
@@ -17,6 +17,8 @@ Array.prototype.equals = function(array) {
 
     return result;
 };
+
+var nil = null; // :)
 
 $(document).ready(function() {
 
@@ -202,46 +204,164 @@ $(document).ready(function() {
     selectors.selectedColumns = 'div#selected-columns';
     selectors.availableColumns = 'div#available-columns';
 
-    $('div.draggable').draggable({
-        revert:'invalid',
-        start: function() {   // We need to track where column has been dragged from initially
-            var $this = $(this);
-            $this.data('dragged-from', $this.parent().attr('id') );
+//    $('div.draggable').draggable({
+//        revert:'invalid',
+//        start: function() {   // We need to track where column has been dragged from initially
+//            var $this = $(this);
+//            $this.data('dragged-from', $this.parent().attr('id') );
+//        }
+//    });
+//
+//    $('div#available-columns').dropable({
+//        accept: '.draggable'
+//    })
+
+//    $('div.droppable').droppable({
+//        accept: '.draggable',
+//        drop: function(event, ui) {
+//
+//            var $this = $(this),
+//                    id = $this.attr('id'),
+//                    column = ui.draggable.text().trim(),
+//                    selectedColumnsAmount = $('div#selected-columns').children('div.draggable').length;
+//
+//            appendDraggable( $this, ui.draggable );
+//
+//            if ( id != ui.draggable.data('dragged-from') ) { // Thanks for a valid drop
+//
+//                if ( id == 'available-columns' ) { // Exclude column
+//                    if ( selectedColumnsAmount > 1 ) {
+//                        toggleColumn(ui.draggable);
+//                    }
+//                    else {
+//                        appendDraggable( $(selectors.selectedColumns), ui.draggable ); // Revert back
+//                    }
+//                } else { // Include column
+//                    toggleColumn(ui.draggable);
+//                }
+//
+//            } else if ( ui.draggable.data('dragged-from') == 'selected-columns' ) {
+//                appendColumn( reportColumn(ui.draggable) );
+//            }
+//        }
+//    });
+
+    $('div#selected-columns, div#available-columns').sortable({
+        connectWith: '.connectedSortable',
+        start: function(event, ui) {
+            ui.item.data('dragged-from', ui.item.parent().attr('id') );
         }
-    });
+//        },
+//        update: function(event, ui) {
+//            var $this = $(this),
+//                id = $this.attr('id'),
+//                column = ui.item.text().trim(),
+//                sender = $('#' + ui.item.data('dragged-from'));
+//
+//            alert('yep');
+//        }
+    }).disableSelection();
 
-    $('div.droppable').droppable({
-        accept: '.draggable',
-        drop: function(event, ui) {
+    $('div#selected-columns').bind('sortupdate', function(event, ui) {
+        var $this = $(this),
+                column = ui.item.text().trim(),
+            // selectedColumnsAmount = $('div#selected-columns').children('div.column-header').length,
+                sender = $('#' + ui.item.data('dragged-from'));
 
-            var $this = $(this),
-                    id = $this.attr('id'),
-                    column = ui.draggable.text().trim(),
-                    selectedColumnsAmount = $('div#selected-columns').children('div.draggable').length;
+        if ( sender.attr('id') == 'selected-columns' && ui.item.parent().attr('id') == 'available-columns' ) {
+            hideColumn( reportColumn(ui.item) )
+        } else {
 
-            appendDraggable( $this, ui.draggable );
-
-            if ( id != ui.draggable.data('dragged-from') ) { // Thanks for a valid drop
-
-                if ( id == 'available-columns' ) { // Exclude column
-                    if ( selectedColumnsAmount > 1 ) {
-                        toggleColumn(ui.draggable);
-                    }
-                    else {
-                        appendDraggable( $(selectors.selectedColumns), ui.draggable ); // Revert back
-                    }
-                } else { // Include column
-                    toggleColumn(ui.draggable);
-                }
-
-            } else if ( ui.draggable.data('dragged-from') == 'selected-columns' ) {
-                appendColumn( reportColumn(ui.draggable) );
+            if ( reportColumn( ui.item ) ) {
+                rebuildTable();
+            } else {
+                serializeFields();
+                $('form#new_report').submit();
             }
+//            var reportRows = $('#report tr').toArray(),
+//                    columns = allIncludedReportColumns();
+//
+//            reportRows.shift(); reportRows.pop();
+//
+//            $.each( columns, function() {
+//
+//                if ( !this.header.is(':visible') ) {
+//                    showColumn(this);
+//                }
+//
+//                $('#report-header').append( this.header );
+//                // alert( $(this.header).attr('abbr') );
+//                var elements = this.elements.toArray();
+//
+//                $.each( reportRows, function() {
+//                    $(this).append( elements.shift() );
+//                });
+//            });
+
         }
+//        $('#report tr').html('');
+//
+//
+//
+//        var reportRows = $('#report tr').toArray();
+//        reportRows.shift(); reportRows.pop();
+//
+//        $.each( columns, function() {
+//
+//            $('#report-header').append( $(this.header) );
+//             // alert( $(this.header).attr('abbr') );
+//            var elements = this.elements.toArray();
+//
+//            $.each( reportRows, function() {
+//                $(this).append( $( elements.shift() ) );
+//            });
+//        });
+
+//        $.each( columns, function() {
+//
+//            var elements = this.elements.toArray();
+//            this.header.show();
+//
+//            $.each( elements, function(index) {
+//                elements[index].show();
+//            })
+//        });
+
+//        var reportRows = $('#report').find('tr').toArray(),
+//                elements = column.elements.toArray();
+//
+//        reportRows.shift(); reportRows.pop(); // Remove header and footer rows
+//
+//        $('#report-header').append( column.header.show() ); // Append header
+//
+//        $.each(reportRows, function() { // append elements
+//            $(this).
+//                    append( $( elements.shift() ).show() );
+//        });
     });
 
 
 });
+
+function rebuildTable() {
+
+    var reportRows = $('#report tr').toArray(),
+            columns = allIncludedReportColumns();
+
+    reportRows.shift(); reportRows.pop();
+
+    $.each( columns, function() {
+
+        if ( !this.header.is(':visible') ) showColumn(this);
+
+        $('#report-header').append( this.header );
+        var elements = this.elements.toArray();
+
+        $.each( reportRows, function() {
+            $(this).append( elements.shift() );
+        });
+    });
+}
 
 function addPagination(){
     $(".paging a").each(function(i, val) {
@@ -265,10 +385,10 @@ function appendDraggable(element, draggable) {
 
 function reportColumn(col) {
     var table = $('#report'),
-        columnNumber,
-        columnName = col.text().trim(),
-        columnHeader,
-        columnElements = [];
+            columnNumber,
+            columnName = col.text().trim(),
+            columnHeader,
+            columnElements = [];
 
     $.each( table.find('th'), function(index, element) {
         if ( $(element).attr('abbr') == columnName ) {
@@ -286,13 +406,37 @@ function reportColumn(col) {
 
 }
 
+function allIncludedReportColumns() {
+    var columns = [],
+            table = $('#report');
+
+//    var columnPosition = function(columnName) {
+//        var result = 0;
+//        $.each( table.find('th'), function(index, element) {
+//            if ( $(element).attr('abbr') == columnName )
+//                result = index + 1;
+//        });
+//        return result;
+//    };
+
+    $.each( $('#selected-columns div.column-header'), function() {
+//        var $element = $(element),
+//                column = {
+//                    header: $element,
+//                    elements: table.find('td:nth-child(' + columnPosition($element.attr('abbr')) + ')')
+//                };
+        columns.push( reportColumn( $(this) ) );
+    });
+    return columns;
+}
+
 function toggleColumn(col) {
 
     var column = reportColumn(col);
 
     synchronizeFields( col );
 
-    if ( column ) { 
+    if ( column ) {
         if ( column.header.is(':visible') ) {
             hideColumn(column);
         } else {
@@ -311,7 +455,7 @@ function toggleColumn(col) {
 
 function appendColumn(column) {
     var reportRows = $('#report').find('tr').toArray(),
-        elements = column.elements.toArray();
+            elements = column.elements.toArray();
 
     reportRows.shift(); reportRows.pop(); // Remove header and footer rows
 
@@ -319,14 +463,14 @@ function appendColumn(column) {
 
     $.each(reportRows, function() { // append elements
         $(this).
-            append( $( elements.shift() ).show() );
+                append( $( elements.shift() ).show() );
     });
 }
 
 function showColumn(column) {
     column.header.show();
     column.elements.show();
-};
+}
 
 function hideColumn(column) {
     column.header.hide();
@@ -366,9 +510,9 @@ function serializeFields() {
 
 function orderChanged() {
     var reportColumns = selectedReportColumns(),
-        serializedColumns = $('input#report_fields').val().split(','),
-        serializedAndVisibleOrder = [],
-        result = false;
+            serializedColumns = $('input#report_fields').val().split(','),
+            serializedAndVisibleOrder = [],
+            result = false;
 
     if ( reportColumns.length == serializedColumns.length ) {
         result = reportColumns.equals( serializedColumns );
